f600d742f6aa0c46f51146635333bd2781fabb56
Mehul <mehulk2007@gmail.com>
2026-03-27 13:37:38 +0000

    feat: add basic types page

--- controllers/typesController.js
+++ controllers/typesController.js
@@ -2,6 +2,7 @@ const db = require("../db/queries");
 
 async function typesGet(req, res) {
   const types = await db.getAllTypes();
+  console.log(types);
   res.render("types", { types });
 }
 
--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,16 @@
 const pool = require("./Pool");
 
 async function getAllTypes() {
-  const { rows } = await pool.query("SELECT * FROM types;");
+  // getting the number of pokemons in each type
+
+  const sql = `
+  SELECT types.name, COUNT(pokemons.name)
+  FROM types 
+  LEFT JOIN pokemons ON pokemons.type_id = types.id
+  GROUP BY types.name;
+  `;
+
+  const { rows } = await pool.query(sql);
   return rows;
 }
 
