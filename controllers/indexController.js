8d9e7952a883fe0e23ca1c7f01fd0b3a5581a3b4
Mehul <mehulk2007@gmail.com>
2026-03-30 04:45:57 +0000

    style: add navbar, style homepage

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,9 +1,21 @@
 const db = require("../db/queries");
 
+const links = [
+  { href: "/", text: "Home" },
+  { href: "/types", text: "Types" },
+  { href: "/pokemons", text: "Pokemons" },
+];
+
+const stats = [
+  { title: "Total Pokemons", value: "151" },
+  { title: "Total Types", value: "12" },
+  { title: "Archive Completion", value: "84%" },
+  { title: "Log Entries", value: "2.3K" },
+];
+
 async function count(req, res) {
-  const counts = await db.getCount();
-  const { tot_types, tot_pokes } = counts[0];
-  res.render("index", { tot_types, tot_pokes });
+  const types = await db.getAllTypes();
+  res.render("index", { types, links, stats });
 }
 
 module.exports = { count };
