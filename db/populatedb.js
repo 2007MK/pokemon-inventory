c85144702ae6602d923c789330cde4b805f4ee3b
Mehul <mehulk2007@gmail.com>
2026-03-30 07:53:56 +0000

    feat: add pokemon images column in db

--- db/populatedb.js
+++ db/populatedb.js
@@ -10,7 +10,8 @@ async function main() {
   CREATE TABLE IF NOT EXISTS pokemons(
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name TEXT,
-  type_id INTEGER);
+  type_id INTEGER,
+  image TEXT);
 
   INSERT INTO types (name) VALUES
   ('water'),
@@ -18,10 +19,10 @@ async function main() {
   ('fire'),
   ('ghost');
 
-  INSERT INTO pokemons (name, type_id) VALUES
-  ('Pikachu', 2),
-  ('Gastly', 4),
-  ('Charmander', 3);
+  INSERT INTO pokemons (name, type_id, image) VALUES
+  ('Pikachu', 2, 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png'),
+  ('Gastly', 4, 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/92.png'),
+  ('Charmander', 3, 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png');
   `;
 
   const url = process.argv[2];
